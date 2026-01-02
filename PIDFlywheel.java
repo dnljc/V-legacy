32b8c1295729d23af70910602696a3f5c76589a4
dnljc <danjaincel@gmail.com>
2026-01-02 11:34:55 -0300

    Inserido telemetry da distancia

--- PIDFlywheel.java
+++ PIDFlywheel.java
@@ -19,40 +19,34 @@ public class PIDFlywheel extends OpMode {
 
     public double P = 0; // TUNAR
     public double F = 0; // TUNAR
-
     GoalAimVelocity goalAimVelocity;
+
 
     @Override
     public void init() {
         fly1 = hardwareMap.get(DcMotorEx.class, "lançador1");
         fly2 = hardwareMap.get(DcMotorEx.class, "lançador2");
-
         fly1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         fly1.setDirection(DcMotorSimple.Direction.FORWARD);
         fly2.setDirection(DcMotorSimple.Direction.REVERSE);
-
         goalAimVelocity = new GoalAimVelocity();
 
-        PIDFCoefficients pidfCoefficients =
-                new PIDFCoefficients(P, 0, 0, F);
-        fly1.setPIDFCoefficients(
-                DcMotor.RunMode.RUN_USING_ENCODER,
-                pidfCoefficients
-        );
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
+        fly1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
     }
 
     @Override
     public void loop() {
-        double targetVel = goalAimVelocity.getTargetVel();
-
-        fly1.setVelocity(targetVel);
-        fly2.setVelocity(targetVel);
+        fly1.setVelocity(goalAimVelocity.getTargetVel());
+        fly2.setVelocity(goalAimVelocity.getTargetVel());
 
         double curVel = fly1.getVelocity();
-        double error = targetVel - curVel;
+        double error = goalAimVelocity.getTargetVel() - curVel;
+        double tVel = goalAimVelocity.getTargetVel();
 
-        telemetry.addData("TARGET VEL", targetVel);
-        telemetry.addData("CURRENT VEL", curVel);
+        telemetry.addData("TARGET VEL: ", tVel);
+        telemetry.addData("CURRENT VEL: ",curVel);
         telemetry.addData("ERROR", error);
+        telemetry.addData("DISTANCE", goalAimVelocity.dis);
     }
 }
